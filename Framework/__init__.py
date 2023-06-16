cd5aad50a7dd9e5ab9f98461bcd57b141562d608
Adolfo Perrusquia <100733638+CKPerrusquia@users.noreply.github.com>
2023-06-16 22:01:17 +0100

    Add files via upload

---
+++ Framework/__init__.py
@@ -0,0 +1,11 @@
+from Framework import (
+    DataGeneration,
+    HybridClassifier,
+    TrajectoryRegression
+     )
+
+__all__ = [
+    "DataGeneration",
+    "HybridClassifier",
+    "TrajectoryRegression"
+]
